fe014ed8d40fc0c6bcc748714e8d2bb485192dc6
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-11 14:05:40 -0800

    C variables tasks

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- *main - determine if number is positive or negative
- * Description: Program will assign a random number when executed
+ *main - print alphabets in lower case except for e and q
+ * Description: Program will print alphabets in lower case without e
+ * and q
  * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-	int n;
+	char alphab;
 
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n >= 1)
-		printf("%d is positive\n", n);
-	else
-		printf("%d is negative\n", n);
+	alphab = 'a';
+	while (alphab <= 'z')
+	{
+		if (alphab != 'e' && alphab != 'q')
+		{
+			putchar(alphab);
+		}
+		alphab++;
+	}
+	putchar(10);
 	return (0);
 
 
