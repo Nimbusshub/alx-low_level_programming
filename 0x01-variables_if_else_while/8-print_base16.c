487166ca86478483a56a8afb4269cd91c32ca71d
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-11 15:47:32 -0800

    C variables tasks

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- *main - determine if number is positive or negative
- * Description: Program will assign a random number when executed
+ * main - print numbers in base 16
+ * Description: Print numbers in base 16
  * Return: Always 0 (Success)
  *
  */
@@ -12,17 +10,18 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n < 103; n++)
+	{
+		putchar(n);
+	}
 
-	n = rand() - RAND_MAX / 2;
-	if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n >= 1)
-		printf("%d is positive\n", n);
-	else
-		printf("%d is negative\n", n);
+	putchar(10);
+
 	return (0);
-
 
 
 }
