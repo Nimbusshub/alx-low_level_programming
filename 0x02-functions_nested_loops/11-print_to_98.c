f3e42b115309b5843d6f802cd6a784c7a4ec8512
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-15 17:10:35 -0700

    Nest loops and functions

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - function that prints all natural numbers
+ * Description: prints all functins
+ * @n: starting imteger
+ * Return: Always 0 (Success)
+ *
+ */
+void print_to_98(int n)
+
+{
+	if (n < 98)
+	{
+		while (l2 < 98)
+		{
+			printf("%d, ", n);
+			n++;
+		}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
+
+}
