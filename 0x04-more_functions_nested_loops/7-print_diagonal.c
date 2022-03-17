25672c7e4f65e66bd28e0afbf5a5a850f04b48f6
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-17 13:43:10 -0700

    Nests loops and functions

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,23 +1,32 @@
 #include "main.h"
 
 /**
- *_islower  - check if alphabets is in lower case
- * @c: c is an ascii character
- * Description:  Program will print 1 if alphabets is in lower case
- * or 0 otherwise
- * Return: 1 if lowercase (Success)
+ * print_diagonal - program prints diagonal line
+ * @n: n is the length of diagonal
+ * Description:  Program will print diagonal line
+ * Return: void (Success)
  *
  */
-int _islower(int c)
+void print_diagonal(int n)
 {
-	if (c >= 'a' && c <= 'z')
+	int line = 0;
+	int spaces;
+
+	if (n > 0)
 	{
-		return (1);
+		while (line < n)
+		{
+			for (spaces = 0; spaces < line; spaces++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			line++;
+		}
 	}
 	else
 	{
-		return (0);
+		_putchar('\n');
 	}
-
 }
-
