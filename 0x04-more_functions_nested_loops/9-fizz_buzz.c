ef9125849b6cf775d437757fd8a9a040faded62e
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-17 19:19:20 -0700

    Nest loops and functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,37 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- *_islower  - check if alphabets is in lower case
- * @c: c is an ascii character
- * Description:  Program will print 1 if alphabets is in lower case
- * or 0 otherwise
- * Return: 1 if lowercase (Success)
- *
+ * main - program will print numbers with letters
+ * Description:  Program will print numbers from 1 to 100, for
+ * multiples of 3, it prints Fizz, for multiples of 5 it prints Buzz
+ * for multiples of 5 and 3, it prints FizzBuzz
+ * Return: Always 0 (Success)
  */
-int _islower(int c)
+int main(void)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	int number = 1;
 
+	while (number <= 100)
+	{
+		if (number % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (number % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", number);
+		}
+		number++;
+	}
+	printf("\n");
+	return (0);
 }
-
