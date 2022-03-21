a4a9d9858f29748482838bd0dbf78da8786d73ee
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-21 14:03:42 -0700

    PAS task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,23 +1,18 @@
 #include "main.h"
 
 /**
- *_isdigit  - check if number is digit 0 to 9
- * @c: c is an ascii character
- * Description:  Program will return 1 if input is digit 0 to 1
- * else it will return 0
- * Return: 1 if digit else 0 (Success)
- *
+ * _strlen - counts and prints the length of string
+ * @s: is a pointer
+ * Return: count (Success)
  */
-int _isdigit(int c)
+int _strlen(char *s)
 {
-	if (c >= 48 && c < 58)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	int count = 0;
 
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
 }
-
