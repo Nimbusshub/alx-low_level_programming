61103a5170efc3d01d47adc500ab8266479c848b
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-21 12:13:39 -0700

    PAS task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,23 +1,16 @@
 #include "main.h"
 
 /**
- *_isdigit  - check if number is digit 0 to 9
- * @c: c is an ascii character
- * Description:  Program will return 1 if input is digit 0 to 1
- * else it will return 0
- * Return: 1 if digit else 0 (Success)
- *
+ * swap_int - swaps two integers to one another
+ * @a: is a pointer
+ * @b: is a pointer
+ * Return: swaps integers (Success)
  */
-int _isdigit(int c)
+void swap_int(int *a, int *b)
 {
-	if (c >= 48 && c < 58)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	int swap;
 
+	swap = *a;
+	*a = *b;
+	*b = swap;
 }
-
