a6f99fd675a4753eb56446906d463f521a0f346f
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-21 18:47:25 -0700

    PSA Task 5

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,16 +15,19 @@ void puts_half(char *str)
 		{
 			break;
 		count++;
-		if (count % 2 == 1)
-		{
-			i = count / 2;
 		}
-		else
-		{
-			i = (count - 1) / 2;
-		}
-		for (i++; i < count; i++)
-		{
-			_putchar(str[i]);
-		_putchar('\n');
+	}
+	if (count % 2 == 1)
+	{
+		i = count / 2;
+	}
+	else
+	{
+		i = (count - 1) / 2;
+	}
+	for (i++; i < count; i++)
+	{
+		_putchar(str[i]);
+	_putchar('\n');
+	}
 }
