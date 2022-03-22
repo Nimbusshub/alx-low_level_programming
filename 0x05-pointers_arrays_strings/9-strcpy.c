948ce42ee37f88dedeeacbbd51789fb39764c91a
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-21 19:23:24 -0700

    PSA Task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- *_isdigit  - check if number is digit 0 to 9
- * @c: c is an ascii character
- * Description:  Program will return 1 if input is digit 0 to 1
- * else it will return 0
- * Return: 1 if digit else 0 (Success)
- *
+ * _strcpy - it copies string length
+ * @dest: is a pointer
+ * @src: is a pointer
+ * Return: dest (Success)
  */
-int _isdigit(int c)
+char *_strcpy(char *dest, char *src)
 {
-	if (c >= 48 && c < 58)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	int i;
 
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		dest[i] = *(src + i);
+	}
+	dest[i] = '\0';
+	return (dest);
 }
+
 
