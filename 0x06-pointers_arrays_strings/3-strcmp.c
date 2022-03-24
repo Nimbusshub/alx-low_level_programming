3084652b7ced557d4856f45bbcba9444fe4245a7
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-23 20:29:27 -0700

    Pointers Arrays and Strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,7 +10,7 @@ int _strcmp(char *s1, char *s2)
 	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
 		s1++;
-		S2++;
+		s2++;
 	}
 	if (*s1 == *s2)
 	{
@@ -18,6 +18,6 @@ int _strcmp(char *s1, char *s2)
 	}
 	else
 	{
-		return (s1 - s2);
+		return (*s1 - *s2);
 	}
 }
