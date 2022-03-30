dd0834f9cff1e2f1f967730b2f5b0648707c899c
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-30 16:15:33 -0700

    Recursion Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,12 @@ int _strlen_recursion(char *s)
 	int len = 0;
 
 	if (*s == '\0')
-	{
 		return (0);
-	}
 	if (*s != '\0')
 	{
 		len = _strlen_recursion(s + 1);
 		len++;
 		return (len);
 	}
+	return (0);
 }
