5921f04d631fb1752d4ede7d23d0a20120f21b06
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-30 17:00:21 -0700

    Recursion Task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * factorial(int n) - print the factors of n
+ * @n: number the factor will start from
+ * Return: the factors (success)
+ */
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n >= 1)
+	{
+		n = n * factorial(n - 1);
+		return (n);
+	}
+	return (0);
+}
