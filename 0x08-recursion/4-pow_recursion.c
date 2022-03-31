c1df1d214a656e002d56f3c8f7db6f496f5fa7ac
Nimota_Busayo <busayonimota1603@gmail.com>
2022-03-30 18:00:56 -0700

    Recursion Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - it prints the power of x to y
+ * @x: the base number
+ * @y: the exponent
+ * Return: the power of x to y
+ */
+int _pow_recursion(int x, int y)
+{
+	int power;
+
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else if
+	{
+		power = x * _pow_recursion(x, y - 1);
+		return (power);
+	}
+	return (0);
+}
