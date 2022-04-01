fec756278ff61ae671c90b18c9a79d16957b78ed
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-01 11:47:27 -0700

    Arguement project task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - a function in c that executes other functions
  * @argc: counts arguements
  * @argv: is the arguement vector
  * Return: return 0 (Success)
  */
+int main(int argc, char *argv[])
+{
+	int i, result = 1;
+
+	if ((argc == 1) || (argc > 3))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		int x = strtol(argv[i], NULL, 10);
+
+		result = result * x;
+	}
+	printf("%d\n", result);
+	return (0);
+}
