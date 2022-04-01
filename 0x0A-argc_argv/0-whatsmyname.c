055965669aad093bbb8d31d14736cf254a6509ed
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-01 08:56:12 -0700

    Arguement project task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,6 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	int i;
-
-	for (i = 0; i <= argc; i++)
-	{
-		printf("%s", argv[i]);
-	}
+	printf("%s\n", argv[0]);
 	return (0);
 }
