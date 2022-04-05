dac611cab8ff82697c04b1b3589ceb067bcf8fcd
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-05 10:33:13 -0700

    malloc free task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenate two strings
+ * @s1: a pointer to a string
+ * @s2: a pointer to string 2
+ * Return: pointer (Success)
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len2 = 0, len1 = 0, index = 0, index2 = 0;
+	char *pointer;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+	pointer = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (pointer == NULL)
+	{
+		free(pointer);
+		return (NULL);
+	}
+	while (index < len1)
+	{
+		*(pointer + index) = *(s1 + index);
+		index++;
+	}
+	while (index < (len1 + len2))
+	{
+		*(pointer + index) = *(s2 + index2);
+		index++;
+		index2++;
+	}
+	return (pointer);
+	free(pointer);
+}
---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - duplicates a string
+ * @str: a pointer to a string
+ * Return: pointer (Success)
+ */
+char *_strdup(char *str)
+{
+	int count = 0, len = 0, index = 0, index2 = 0;
+	char *pointer;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[index] != '\0')
+	{
+		count++;
+		index++;
+	}
+	len = count;
+	pointer = (char *)malloc(sizeof(char) * (len + 1));
+
+	if (pointer == NULL)
+		return (NULL);
+	while (index2 < len)
+	{
+		*(pointer + index2) = *(str + index2);
+		index2++;
+	}
+	return (pointer);
+	free(pointer);
+}
