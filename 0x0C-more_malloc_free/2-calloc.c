1dce36f2c9da101404f45065ad650d57534bc08e
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-07 18:12:59 -0700

    more mallocs and free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
-	pointer = malloc(nmemb * size);
+	pointer = (int *)malloc(nmemb * size);
 	if (pointer == NULL)
 		return (NULL);
 	while (i < *(pointer + nmemb))
