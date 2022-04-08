81f8b591a318df8dfdd9292045a4f63adf6cd288
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-07 18:04:20 -0700

    more mallocs and free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc - a funtion that allocates memory blocks of nmemb
+ * in the heap
+ * @nmemb: the size_t nmemb arguement of calloc function
+ * @size: size of the datatype
+ * Return: the memory address of calloc or return NULL
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *pointer = NULL;
+	int i = 0;
+
+	if ((nmemb == 0) || (size == 0))
+		return (NULL);
+	pointer = malloc(nmemb * size);
+	if (pointer == NULL)
+		return (NULL);
+	while (i < *(pointer + nmemb))
+	{
+		*(pointer + i) = 0;
+		i++;
+	}
+	return (pointer);
+}
