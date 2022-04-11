e35b8247590524d38e57c0f974039ba1bada6ced
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-11 16:44:59 -0700

    Structures and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @owner: third member
  * Description: no long cap
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
