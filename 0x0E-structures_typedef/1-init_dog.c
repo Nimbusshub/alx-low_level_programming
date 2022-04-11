8be52eec69db63294bfc942a4508d334e616b1b0
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-11 10:31:26 -0700

    Structures and typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - void function that initializes a variable
+ * @d: pointer to the address of the initialized variable
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Return: return an initialized variable.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
