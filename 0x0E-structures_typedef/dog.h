874554ca921400f7b427720694f55758e90d8b48
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-11 09:02:23 -0700

    Structures and typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - a new struct with three members
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ * Description: no long cap
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
