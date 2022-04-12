aade11f68e115fe4695f4a9542f5f43ff4212999
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-12 07:35:43 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - the main function in c
+ * @void: a void arguement
+ * Return: the full path name of the file
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
