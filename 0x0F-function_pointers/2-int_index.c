a19ec051cf922c3f805e651d0bb7ec1bf4f27540
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-13 07:21:48 -0700

    Function pointers

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for the first index of an array that
+ * matches what the function pointers points to
+ * @array: the array of numbers to search through and compare with
+ * @size: size of the array
+ * @cmp: a funtion pointer
+ * Return: the index of the array (Success) else return -1 or 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if ((cmp == NULL) || (array == NULL))
+		return (0);
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if ((*cmp)(array[i]))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
