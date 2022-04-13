059c1cab5056fcebdf355703fbf6f983e88e0ed3
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-13 07:28:40 -0700

    Function pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i = 0;
 
 	if ((cmp == NULL) || (array == NULL))
-		return (0);
+		return;
 	if (size <= 0)
 		return (-1);
 	while (i < size)
