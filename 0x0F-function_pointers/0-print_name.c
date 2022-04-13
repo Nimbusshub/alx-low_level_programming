623584f9009c8bd7787d4bf543d95a93ecfaff1a
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-13 03:40:30 -0700

    Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - a callback function
+ * @name: a pointer to a character arguement
+ * @f: a funtion pointer
+ * Return: a printed name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
