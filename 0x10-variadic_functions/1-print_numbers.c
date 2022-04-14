8a546a93f0fcaaea6d2879b0fba330c996bad100
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-14 08:37:21 -0700

    Variadic Functions4

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - it prints all the numbers passed into its arguements
+ * @separator: it seperates arguements
+ * @n: the number of inputs
+ * Return: numbers (Success)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list numbers;
+
+	if (n == 0)
+		return;
+	va_start(numbers, n);
+	while (i < n)
+	{
+		printf("%d%s", va_arg(numbers, int), separator);
+		i++;
+	}
+	printf("\n");
+	va_end(numbers);
+}
