91c66bc670223793751ebb214f6a6a28f4e276a0
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-22 01:06:17 -0700

    Singly linked list task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t linked list
+ * @head: the pointer to the first node in a linked list
+ * Return: free all nodes (Success)
+ */
+void free_list(list_t *head)
+{
+	list_t *new_node = NULL;
+
+	while (head)
+	{
+		new_node = head;
+		head = head->next;
+
+		free(new_node->str);
+		new_node->str = NULL;
+
+		free(head);
+		free(new_node);
+	}
+}
