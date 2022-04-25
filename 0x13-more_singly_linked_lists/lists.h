d42d11d3507e425c90c416c5427adeb15d02cf13
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-25 09:21:57 -0700

    add new nodes at the end of a listint_t SLL

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -22,5 +23,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif /* _LISTS_H_ */
