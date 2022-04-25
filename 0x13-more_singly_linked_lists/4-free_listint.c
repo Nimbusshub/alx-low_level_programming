55f609516aa30462102806678bfb73cb2b1bb1a4
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-25 10:38:41 -0700

    free all allocated memory blocks of listint_t SLL

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t SLL
+ * @head: pointer to the first node
+ * Return: NULL.
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *agent_clear, *temp = head;
+
+	while (temp->next != NULL)
+	{
+		agent_clear = temp;
+		temp = temp->next;
+		free(agent_clear);
+	}
+	head = NULL;
+	free(temp);
+}
