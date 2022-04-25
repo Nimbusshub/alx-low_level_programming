963b110008f86f648e9dae365d126bb7903e4b58
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-25 13:41:38 -0700

    free DAM and initializes head to NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t SLL and sets head to NULL
+ * @head: pointer to the first node
+ * Return: NULL.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *agent_clear, *temp = *head;
+
+	while (temp != NULL)
+	{
+		agent_clear = temp;
+		temp = temp->next;
+		free(agent_clear);
+	}
+	*head = NULL;
+	free(temp);
+}
