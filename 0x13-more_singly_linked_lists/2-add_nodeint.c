519657121868e6e4d57943fb62c299c95df0f280
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-25 08:36:48 -0700

    add new nodes in a listint_t SLL

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds new nodes at the begining of listint_t list
+ * @head: pointer to head which holds the add of the first node
+ * @n: the value to initialise the elements in the nodes with
+ * Return: add of the new node (Sucess) or NULL if failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+
+	new = (listint_t *)malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
