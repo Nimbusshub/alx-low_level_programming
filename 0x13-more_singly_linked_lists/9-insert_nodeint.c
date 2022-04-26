3dda40779f9f9ec7b9e5c907b1159c8bf78287b4
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-26 09:07:09 -0700

    insert new node at a given index #fix 3

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *tmp;
 	unsigned int i = 0;
 
-	if (head == NULL && idx != 0)
-		return (NULL);
 	if (head)
 	{
 		new_node = (listint_t *)malloc(sizeof(listint_t));
