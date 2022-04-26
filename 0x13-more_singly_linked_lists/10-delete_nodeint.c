b295ea8eb8abb5bb4a0256da9d06ddd4b712c9a4
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-26 13:03:59 -0700

    delete a listint_t at the index passed in

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes node  at index
+ * @head: pointer to the head node
+ * @index: index to delete from
+ * Return: 1 (success) -1 (failed)
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *agent_clear, *prevV;
+	unsigned int i = 0;
+
+	if ((head == NULL) || (*head == NULL))
+		return (-1);
+	if (head)
+	{
+		if (index == 0)
+		{
+			tmp = *head;
+			*head = (*head)->next;
+			free(temp);
+		}
+
+		else
+		{
+			agent_clear = *head;
+			for (; i < index - 1 && agent->next != NULL; i++)
+			{
+				prevV = agent_clear;
+				agent_clear = agent_clear->next;
+			}
+			temp = agent_clear->next;
+			prevV->next = temp;
+			free(agent_clear);
+		}
+		return (1);
+	}
+	return (0);
+}
