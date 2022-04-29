7c219e03375aaa14e938174529bbbe7490244831
Nimota_Busayo <busayonimota1603@gmail.com>
2022-04-28 18:13:06 -0700

    convert binary to an unsigned int #Comp err fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -24,3 +24,25 @@ unsigned int binary_to_uint(const char *b)
 	}
 	return (sum);
 }
+
+/**
+ * _pow - it prints the power of x to y
+ * @x: the base number
+ * @y: the exponent
+ * Return: the power of x to y
+ */
+int _pow(int x, int y)
+{
+	int power;
+
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else if (y >= 1)
+	{
+		power = x * _pow(x, y - 1);
+		return (power);
+	}
+	return (0);
+}
