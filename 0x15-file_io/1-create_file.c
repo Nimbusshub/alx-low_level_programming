1a009fed61a748c93be0a63a404f6daeb39ec9b0
Nimota_Busayo <busayonimota1603@gmail.com>
2022-05-03 10:37:34 -0700

    create new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wCount = write(filename, text_content, strlen(text_content));
+	wCount = write(file, text_content, strlen(text_content));
 	if (wCount < 0)
 		return (-1);
 	close(file);
