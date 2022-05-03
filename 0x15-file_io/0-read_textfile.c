2621574a42b4101c18d28d48daad7a0838c24e6e
Nimota_Busayo <busayonimota1603@gmail.com>
2022-05-03 08:49:40 -0700

    read a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * read_textfile - reads a file and write it to stdout
+ * @filename: file to read and print to stdout
+ * @letters: numbers of chars to read
+ * Return: the numbers of letters it could read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file, i;
+	long int m;
+	char *s;
+
+	s = malloc(sizeof(char) * letters + 1);
+
+	if (!filename)
+	{
+		free(s);
+		return (0);
+	}
+
+	file = open(filename, O_RDONLY);
+	m = read(file, s, letters);
+	s[m] = '\0';
+	
+	i = write(STDIN_FILENO, s, letters);
+	if (i < 0 || m != i)
+	{
+		free(s);
+		return (0);
+	}
+	close(file);
+	free(s);
+	return (m);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,20 @@
+# C - File I/O #
+
+  This is a project on file input and output in C. 
+
+#### `0-read_textfile.c` ####
+
+ A function that reads a text file and prints it to the POSIX standard output.
+
+#### `1-create_file.c` ####
+
+  A function that creates a file.
+
+#### `2-append_text_to_file.c` ####
+
+  A function that appends texts to a file
+
+#### `3-cp.c` ####
+
+  A program that copies the content of a file to another file.
+
