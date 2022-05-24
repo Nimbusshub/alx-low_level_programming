445b45ef142dc21fc8dc6fd2f6a5f00dfa939091
Nimah <busayonimota1603@gmail.com>
2022-05-24 18:12:31 +0100

    [Fix] print with right spacing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,20 +11,21 @@ void print_times_table(int n)
 	int i, j, r;
 
 	if (n >= 0 && n <= 15)
-
+	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= n; j++)
+			printf("0");
+			for (j = 1; j <= n; j++)
 			{
 				r = i * j;
-				printf("%d", r);
-				if (i < n + 1 && j < n)
-				{
-					putchar(',');
-					printf("   ");
-				}
+				if (r <= 9)
+					printf(",   %d", r);
+				else if (r >= 10 && r <= 99)
+					printf(",  %d", r);
+				else if (r >= 100 && r <= 999)
+					printf(", %d", r);
 			}
 			putchar('\n');
 		}
-
+	}
 }
