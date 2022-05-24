2b439827d3b584e9652fa7427543b5900c45d28a
Nimah <busayonimota1603@gmail.com>
2022-05-24 15:37:39 +0100

    [Refactor] print times table n number of times

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints times tables n times
+ * @n: number of times to print the times table
+ * Return: print the times table no of n times
+ */
+
+void print_times_table(int n)
+{
+	int i, j, r;
+
+	if (n >= 0 && n <= 15)
+
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				r = i * j;
+				printf("%d", r);
+				if (i < n + 1 && j < n)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			putchar('\n');
+		}
+
+}
