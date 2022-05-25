821dd3374c455e182b34f5a7adf4d25bbbbae656
Nimah <busayonimota1603@gmail.com>
2022-05-25 21:41:12 +0100

    [Refactor] print the largest prime factor of a number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * main - prints the main program
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	long int container = 612852475143;
+	long int i = 2;
+	int prime = 0;
+
+	while (i <= container)
+	{
+		if ((container % i) == 0)
+		{
+			container /= i;
+			i--;
+		}
+		i++;
+	}
+
+	printf("%ld\n", i);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -9,3 +16,5 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+
+#endif /* _MAIN_H_ */
