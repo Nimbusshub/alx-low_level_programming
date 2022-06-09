d8a6d18d2e4dd53036273616e2fa254299fe9f9b
Nimah <busayonimota1603@gmail.com>
2022-06-09 21:23:48 +0100

    fixed some issue in 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,7 +10,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *head1 = *h, *head2, *new_node;
+	dlistint_t *head1 = *h, *head2 = NULL, *new_node;
 	unsigned int i = 0;
 	int flag = 0;
 
