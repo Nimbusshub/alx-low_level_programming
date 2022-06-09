906668bed988933e9d20bccda1a080a94cb7d162
Nimah <busayonimota1603@gmail.com>
2022-06-09 18:00:17 +0100

    updated the previous node to point to the new_node

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,14 +17,14 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new_node->n = n;
 	new_node->prev = NULL;
-	
+
 	if (*head == NULL)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	
+
 	new_node->next = head1;
 	head1->prev = new_node;
 	*head = new_node;
