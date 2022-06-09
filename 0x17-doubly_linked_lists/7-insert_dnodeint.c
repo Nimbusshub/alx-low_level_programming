454cc7261e48322d4cb1dabadd48a68ea1f47293
Nimah <busayonimota1603@gmail.com>
2022-06-09 21:18:20 +0100

    add a new node at the specified index of a doubly linked list

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at the specified index
+ * @h: the first node
+ * @idx: the index to insert the node
+ * @n: the new element
+ * Return: the doubly linked list
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *head1 = *h, *head2, *new_node;
+	unsigned int i = 0;
+	int flag = 0;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (!h)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	while (head1 != NULL)
+	{
+		if (i == idx)
+		{
+			flag = 1;
+			break;
+		}
+		head2 = head1;
+		head1 = head1->next;
+		i++;
+	}
+	if (flag == 0)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = head1;
+	new_node->prev = head1->prev;
+	head1->prev = new_node;
+	head2->next = new_node;
+
+	return (new_node);
+}
