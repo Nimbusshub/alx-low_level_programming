fdef1c2b34e11739a7acb14b6d57a07011789533
Nimah <busayonimota1603@gmail.com>
2022-06-13 21:18:32 +0100

    c dynamic library that can be called with python

---
+++ 0x18-dynamic_libraries/bussy.h
@@ -0,0 +1,12 @@
+#ifndef _BUSSY_H_
+#define _BUSSY_H_
+
+#include <stdio.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif /* _BUSSY_H_ */
